0094f9e430bd5b8a0e322b911faac7d325c4d500
jesse <nitchsx@gmail.com>
2025-09-01 18:48:54 -0300

    feat: add Gradle migration settings and enhance Kotlin examples

--- estruturas_de_controle/src/main/kotlin/Main.kt
+++ estruturas_de_controle/src/main/kotlin/Main.kt
@@ -1,12 +1,20 @@
 package Conceitos_basicos
 
 import java.util.Queue
+import kotlin.math.ln
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    whenStatement()
-    println(whenExpression(1))
+//    whenStatement()
+//    println(whenExpression(1))
+//    forLoop()
+
+//    whileAndDoLoop()
+    ranges()
+
+    fun max(a: Int, b: Int) = if (a > b) a else b // if expression
+    println(max(2,3))
 }
 
 
@@ -30,3 +38,68 @@ fun whenStatement() {
         else -> print("User role is not recognized")
     }
 }
+
+
+//for
+fun forLoop() {
+    val cakes = listOf("carrot", "chocolate", "vanilla")
+
+    for (cake in cakes) {
+        println("I like $cake")
+    }
+}
+
+
+fun whileAndDoLoop(){
+    fun eatACaked() = println("Eat a Cake")
+    fun cakesBaked() = println("Bake a Cake")
+
+    var cakesEaten = 0
+    var cakesBaked = 0
+
+    while(cakesEaten < 5){
+        eatACaked()
+        cakesEaten++
+    }
+
+    do{
+        cakesBaked()
+        cakesBaked++
+    }while(cakesBaked < cakesEaten)
+}
+
+fun iterators(){
+
+}
+
+fun ranges() {
+    for (i in 0..5) { // de 0 a 5
+        println(i)
+    }
+
+    for (i in 0 until 5) { // de 0 a 4 , until serve pra dizer que n chega no 5
+        println(i)
+    }
+
+    for (i in 2..10 step 2) { // de 2 a 10 de 2 em 2
+        println(i)
+    }
+
+    for (i in 5 downTo 0) { // de 5 a 0
+        println(i)
+    }
+
+    for (c in 'a'..'k') { // de a a k
+        println(c)
+    }
+}
+
+fun igualdade(){
+    val authors = setOf("Shakespeare", "Hemingway", "Twain")
+    val writers = setOf("Shakespeare", "Hemingway", "Twain")
+
+    println(authors == writers) // true (mesmo conteudo) // comparação estrutural
+    println(authors === writers) // false (objetos diferentes , pois tem referencias de memorias
+//diferentes  ) // comparação referencial
+}
+
