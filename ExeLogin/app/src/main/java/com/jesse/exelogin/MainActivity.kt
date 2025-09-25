255dbb1b981c2e1b98934923d0eb5d1d7ff32744
jesse <nitchsx@gmail.com>
2025-09-25 15:43:52 -0300

    feat: update UI components and implement login functionality with data binding

--- ExeLogin/app/src/main/java/com/jesse/exelogin/MainActivity.kt
+++ ExeLogin/app/src/main/java/com/jesse/exelogin/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.jesse.exelogin
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.jesse.exelogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+        private lateinit var binding: ActivityMainBinding
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        setContentView(binding.root)
+        binding.buttonLogin.setOnClickListener { clickLogin() }
+    }
+
+
+
+    fun clickLogin(){
+        val email = binding.username.text.toString()
+        val password = binding.password.text.toString()
+
+        if(email == "cice@gmail.com" && password == "amour"){
+            AlertDialog.Builder(this)
+                .setTitle("Te amo um tantão emmm ")
+                .setMessage("Bem vindo ao sistema")
+                .setPositiveButton("OK"){ dialog, which ->
+                    dialog.dismiss()
+                }
+                .show()
+        }else{
+            Toast.makeText(this, "Usuário ou senha inválidos", Toast.LENGTH_SHORT).show()
         }
     }
 }
